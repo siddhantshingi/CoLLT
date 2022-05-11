9165599bb87a0db7fa3c730fea2fe18fbe88631a
mehektulsyan <mehekteen16@gmail.com>
2022-05-12 03:57:34 +0530

    Update augmentors.py
    RandomSampling augmentation

--- augmentors.py
+++ augmentors.py
@@ -33,3 +33,20 @@ class Identity(Augmentor):
     def augment(self, x: Dataset, device='cpu') -> Dataset:
         device = torch.device(device)
         return get_model_inputs(x, device)
+
+class RandomSampling(Augmentor):
+    def __init__(self):
+        super(RandomSampling, self).__init__()
+
+    def augment(self, x: Dataset, device='cpu') -> Dataset:
+        ids, mask = [], []
+        for data in x:
+          l = len(data['text'])
+          upper = l - 510
+          random = random.randint(1, upper)
+          ids.append([data['input_ids'][0]] + data['input_ids'][random:511] + [data['input_ids'][l-1]])
+          mask.append([data['attention_mask'][0]] + data['attention_mask'][random:511] + data['attention_mask'][l-1])
+        device = torch.device(device)
+        ids_tensor = torch.tensor(ids).to(device)
+        mask_tensor = torch.tensor(mask).to(device)
+        return ids_tensor, mask_tensor
