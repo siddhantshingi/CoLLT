f8ab2e3a71022d7b4c3f72d9965a0fd592a6f082
Alok Kumar <alokrkmv12@gmail.com>
2022-05-12 18:35:15 -0400

    Baselines and test data

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from transformers import BertForSequenceClassification, DistilBertForSequenceClassification
-from transformers import BertTokenizerFast, DistilBertTokenizerFast
+from transformers import LongformerForSequenceClassification, BertForSequenceClassification, DistilBertForSequenceClassification, RobertaForSequenceClassification,RobertaConfig
+from transformers import LongformerTokenizerFast, BertTokenizerFast, DistilBertTokenizerFast, RobertaTokenizer
 
 def get_encoder(num_classes, model='distilbert', device='cpu'):
     if model=='bert':
@@ -18,4 +18,14 @@ def get_encoder(num_classes, model='distilbert', device='cpu'):
             output_hidden_states = False, # Whether the model returns all hidden-states.
         )
         tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased', do_lower_case=True)
+    elif model=='longformer':
+        model = LongformerForSequenceClassification.from_pretrained('allenai/longformer-base-4096',
+                                                           gradient_checkpointing=False,
+                                                           attention_window = 512)
+        tokenizer = LongformerTokenizerFast.from_pretrained('allenai/longformer-base-4096', max_length = 1024)
+    elif model=='roberta':
+      config = RobertaConfig.from_pretrained('roberta-base')
+      tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+      model = RobertaForSequenceClassification(config)
+
     return model.to(device), tokenizer
